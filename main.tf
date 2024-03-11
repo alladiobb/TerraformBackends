76137c6db3ace51e6ef4c066034f1f2c47353a20
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-03-11 20:15:01 -0300

    Refact: gitignore (+terrafor.*) and main (password)

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = "var.access_key"
+  secret_key = "var.secret_key"
 }
 
 resource "aws_instance" "instance1"{
