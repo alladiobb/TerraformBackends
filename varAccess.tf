94c5c2b7bc187328bff3e95e938bd3d944c6794e
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-03-11 19:44:07 -0300

    Add: varAccess.tf

---
+++ varAccess.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description   = "Your AWS Access Key"
+  default       = "A5465A8A9AA5A46"
+  type          = string
+}
+
+variable "secret_key" {
+  description = "Your AWS Secret Key"
+  default     = "13d5sa15dsa68ds1a8d1as66"
+  type        = string
+}
